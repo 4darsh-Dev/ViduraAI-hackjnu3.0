7fc7e2564f548525ce465d52454f53ebe5f42732
adarsh <adarshmaurya9415@gmail.com>
2024-01-28 07:51:39 +0530

    udpated learning features

--- home/models.py
+++ home/models.py
@@ -6,11 +6,10 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(max_length=122)
     email = models.CharField(max_length=122)
-    phone = models.CharField(max_length=12)
+    # phone = models.CharField(max_length=12)
     desc = models.TextField()
     date = models.DateField()
     
     def __str__(self):
         return f"Message from {self.name}"
-    
     
